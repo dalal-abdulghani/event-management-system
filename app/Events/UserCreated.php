1cd8792e83ca7ba1806d144efa2c8ed559a02d2e
dalal-abdulghani <dalal.m.abdulghani@gmail.com>
2025-09-23 14:19:43 +0300

    feat(user): Add user model, migration, factory, and seeder
    Automated commit by setup.sh

---
+++ app/Events/UserCreated.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Events;
+
+class UserCreated
+{
+    // Placeholder event for user
+}
---
+++ app/Listeners/UserCreatedListener.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Listeners;
+
+class UserCreatedListener
+{
+    // Placeholder listener for user
+}
